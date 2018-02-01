d62369457a82922922a1a5cf3ada8395dd92d9f8
simpleplanya <simpleplanya@gmail.com>
2018-02-01 17:30:41 +0800

    update example

--- bulePrint_hello_example/hello/views.py
+++ bulePrint_hello_example/hello/views.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
 
 blueprint = Blueprint('hello',__name__)
+blueprint = Blueprint('teste',__name__)
 
 @blueprint.route("/")
 
 def hello():
     return "hello World"
+
+@blueprint.route("/test")
+
+def teste():
+    return "test-area"
