766b2196b50b7fdc390fca774385e80e5e30c40b
simpleplanya <simpleplanya@gmail.com>
2018-03-03 15:16:49 +0800

    connet_pymongo

--- bulePrint_hello_example/db/__init__.py
+++ bulePrint_hello_example/db/__init__.py
@@ -1,7 +1,7 @@
-from pymongo  import Connection
+from pymongo  import MongoClient
 
 if __name__ == '__main__':
-    con = Connection()
-    db = con.test_database
-    people = db.people
-
+    db = MongoClient(host='localhost:27017',replicaset=None)['Jq']
+    con = db['testData']
+    x = db.testData.find({})
+    print(list(con.find()))
